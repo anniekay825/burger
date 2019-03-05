aa827c1b7927c5e03c441e299417812b9a4c4c60
anniekay825@gmail.com <anniekay825@gmail.com>
2019-03-04 21:10:00 -0500

    updated files

--- config/connection.js
+++ config/connection.js
@@ -1,15 +1,16 @@
 var mysql = require("mysql");
 
-if (process.env.JAWSDB_URL) {
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
-} else {
-    connection = mysql.createConnection({
+// if (process.env.JAWSDB_URL) {
+//     connection = mysql.createConnection(process.env.JAWSDB_URL);
+// } else {
+
+var connection = mysql.createConnection({
+        port: 3306,
         host: "localhost",
         user: "root",
         password: "root",
         database: "burgers_db"
-    });
-};
+});
 
 connection.connect(function(err) {
     if (err) {
